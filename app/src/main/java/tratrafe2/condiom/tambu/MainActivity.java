e65a13a57fdfa0ec35b9788fbc6e1559686e82c1
condiom <constantinosdiomedous@live.com>
2017-01-10 20:56:32 +0200

    fist working test

--- app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
+++ app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
@@ -1,13 +1,46 @@
 package tratrafe2.condiom.tambu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Scanner;
 
+public class MainActivity extends Activity {
+
+    String[][] text;
+    TextView txt1;
+    TextView txt2;
+    TextView txt3;
+    TextView txt4;
+    TextView txtMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = new String[200][5];
+        txt1 = (TextView) findViewById(R.id.txt1);
+        txt2 = (TextView) findViewById(R.id.txt2);
+        txt3 = (TextView) findViewById(R.id.txt3);
+        txt4 = (TextView) findViewById(R.id.txt4);
+        txtMain = (TextView) findViewById(R.id.txtMain);
+        Scanner sc = null;
+        try {
+            sc = new Scanner(getResources().openRawResource(R.raw.words));
+
+        } catch (Exception e) {
+
+        }
+        for (int i = 0; i < 200; i++) {
+            for (int j = 0; j < 5; j++) {
+                text[i][j] = sc.next();
+            }
+        }
+        txtMain.setText(text[0][0]);
+        txt1.setText(text[0][1]);
+        txt2.setText(text[0][2]);
+        txt3.setText(text[0][3]);
+        txt4.setText(text[0][4]);
+
     }
 }
