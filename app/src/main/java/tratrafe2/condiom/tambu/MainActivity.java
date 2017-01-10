c4845ae5f58160031aad6c7f240c5f316e608977
condiom <constantinosdiomedous@live.com>
2017-01-10 21:11:34 +0200

    afd

--- app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
+++ app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
@@ -2,6 +2,7 @@ package tratrafe2.condiom.tambu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TextView;
 
 import java.util.Scanner;
@@ -36,6 +37,15 @@ public class MainActivity extends Activity {
                 text[i][j] = sc.next();
             }
         }
+        float sizeOfLetters = 30;
+        txtMain.setTextSize(sizeOfLetters + 15);
+        txt1.setTextSize(sizeOfLetters);
+        txt2.setTextSize(sizeOfLetters);
+        txt3.setTextSize(sizeOfLetters);
+        txt4.setTextSize(sizeOfLetters);
+
+        txt3.setGravity(Gravity.CENTER_HORIZONTAL);
+
         txtMain.setText(text[0][0]);
         txt1.setText(text[0][1]);
         txt2.setText(text[0][2]);
