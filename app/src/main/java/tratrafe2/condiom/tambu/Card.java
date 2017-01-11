58732ceff1feaceaed18a8d4f5335ade5117d9e0
tratrafe2 <tratrafe2@gmail.com>
2017-01-11 15:11:38 +0200

    created object Cards timer is not functional

---
+++ app/src/main/java/tratrafe2/condiom/tambu/Card.java
@@ -0,0 +1,55 @@
+package tratrafe2.condiom.tambu;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * Created by tratrafe2 on 11/01/2017.
+ */
+
+public class Card {
+    String word;
+    String[] apagorevmenes;
+
+    public Card() {
+        word = "";
+        apagorevmenes = new String[4];
+    }
+
+    public Card[] initArray(Context c) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(c.getResources().openRawResource(R.raw.words));
+        } catch (Exception e) {
+        }
+        boolean check=true;
+        List<Card> list=new ArrayList<>();
+        while(sc.hasNext()){
+            Card temp=new Card();
+            temp.word=sc.nextLine();
+            for(int i=0;i<4;i++){
+                if(sc.hasNext()) {
+                    temp.apagorevmenes[i] = sc.nextLine();
+                }else{
+                    check=false;
+                }
+            }
+           if(check)
+               list.add(temp);
+        }
+        Card[] array=new Card[list.size()];
+        for(int i=0;i<list.size();i++){
+            array[i]=list.get(i);
+        }
+        return array;
+    }
+}
--- app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
+++ app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
@@ -2,55 +2,65 @@ package tratrafe2.condiom.tambu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Gravity;
 import android.widget.TextView;
 
 import java.util.Scanner;
 
 public class MainActivity extends Activity {
-
-    String[][] text;
+    TextView timer;
     TextView txt1;
     TextView txt2;
     TextView txt3;
     TextView txt4;
     TextView txtMain;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = new String[200][5];
         txt1 = (TextView) findViewById(R.id.txt1);
         txt2 = (TextView) findViewById(R.id.txt2);
         txt3 = (TextView) findViewById(R.id.txt3);
         txt4 = (TextView) findViewById(R.id.txt4);
+        timer = (TextView) findViewById(R.id.timer);
         txtMain = (TextView) findViewById(R.id.txtMain);
-        Scanner sc = null;
-        try {
-            sc = new Scanner(getResources().openRawResource(R.raw.words));
 
-        } catch (Exception e) {
-
-        }
-        for (int i = 0; i < 200; i++) {
-            for (int j = 0; j < 5; j++) {
-                text[i][j] = sc.next();
-            }
-        }
+        Card temp = new Card();
+        Card[] cardArray = temp.initArray(this);
+        System.out.println("hello");
         float sizeOfLetters = 30;
         txtMain.setTextSize(sizeOfLetters + 15);
         txt1.setTextSize(sizeOfLetters);
         txt2.setTextSize(sizeOfLetters);
         txt3.setTextSize(sizeOfLetters);
         txt4.setTextSize(sizeOfLetters);
+        timer.setTextSize(sizeOfLetters);
 
         txt3.setGravity(Gravity.CENTER_HORIZONTAL);
+        int i = 50;
+            startCounter();
+            txtMain.setText(cardArray[i].word);
+            txt1.setText(cardArray[i].apagorevmenes[0]);
+            txt2.setText(cardArray[i].apagorevmenes[1]);
+            txt3.setText(cardArray[i].apagorevmenes[2]);
+            txt4.setText(cardArray[i].apagorevmenes[3]);
+        }
 
-        txtMain.setText(text[0][0]);
-        txt1.setText(text[0][1]);
-        txt2.setText(text[0][2]);
-        txt3.setText(text[0][3]);
-        txt4.setText(text[0][4]);
 
+    public void startCounter(){
+        countDownTimer: new CountDownTimer(60*1000,1000){
+            @Override
+            public void onTick(long milisUntilFinished){
+                timer.setText(""+milisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timer.setText("DONE");
+            }
+        };
     }
 }
