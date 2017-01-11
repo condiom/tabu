5500d3f1636fc831201f5dc075a46f4f1a77b3cc
tratrafe2 <tratrafe2@gmail.com>
2017-01-11 18:32:52 +0200

    fixed timer+red. added comments added TODO.

--- app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
+++ app/src/main/java/tratrafe2/condiom/tambu/MainActivity.java
@@ -1,8 +1,10 @@
 package tratrafe2.condiom.tambu;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.test.RenamingDelegatingContext;
 import android.view.Gravity;
 import android.widget.TextView;
 
@@ -30,7 +32,7 @@ public class MainActivity extends Activity {
 
         Card temp = new Card();
         Card[] cardArray = temp.initArray(this);
-        System.out.println("hello");
+
         float sizeOfLetters = 30;
         txtMain.setTextSize(sizeOfLetters + 15);
         txt1.setTextSize(sizeOfLetters);
@@ -41,26 +43,53 @@ public class MainActivity extends Activity {
 
         txt3.setGravity(Gravity.CENTER_HORIZONTAL);
         int i = 50;
-            startCounter();
-            txtMain.setText(cardArray[i].word);
-            txt1.setText(cardArray[i].apagorevmenes[0]);
-            txt2.setText(cardArray[i].apagorevmenes[1]);
-            txt3.setText(cardArray[i].apagorevmenes[2]);
-            txt4.setText(cardArray[i].apagorevmenes[3]);
-        }
+        startCounter(60);
+        //TODO: na valoume on button click, na ginete random ena i j na kali tin printCard
+        printCard(cardArray, i);
+    }
 
+    /**
+     * Tiponi ta periexomena tis kartas [i] pou ton pinaka cardArray.
+     * isos nan kalitera o pinakas nan lista.
+     * {
+     *  lista -> o(N) gia na e3agis stixio
+     *         -> mporoume kathe karta p ethkiavastike na tin kamnoume remove p ti lista
+     * pinakas -> O(1) gia na thkiavasis stixio
+     *          ->kratoume 1 pointer sto telos tou array. kathe fora p thkiavazoume le3i,
+     *            kamnoume swap me to stixio ston pointer, j katevazoume ton pointer kata 1.
+     * }
+     *
+     * @param cardArray
+     * @param i
+     */
+    public void printCard(Card[] cardArray, int i) {
+        txtMain.setText(cardArray[i].word);
+        txt1.setText(cardArray[i].apagorevmenes[0]);
+        txt2.setText(cardArray[i].apagorevmenes[1]);
+        txt3.setText(cardArray[i].apagorevmenes[2]);
+        txt4.setText(cardArray[i].apagorevmenes[3]);
+    }
 
-    public void startCounter(){
-        countDownTimer: new CountDownTimer(60*1000,1000){
+    /**
+     * Metritis pros ta katw arxizontas p to minutes.
+     *
+     * @param minutes
+     */
+    public void startCounter(int minutes) {
+        countDownTimer:
+        new CountDownTimer(minutes * 1000, 10) {
             @Override
-            public void onTick(long milisUntilFinished){
-                timer.setText(""+milisUntilFinished/1000);
+            public void onTick(long milisUntilFinished) {
+                timer.setText(String.format("%d:%03d", milisUntilFinished / 1000, milisUntilFinished % 1000));
+                if (milisUntilFinished / 1000 <= 10) {
+                    timer.setTextColor(Color.RED);
+                }
             }
 
             @Override
             public void onFinish() {
-                timer.setText("DONE");
+                timer.setText("TIME'S UP");
             }
-        };
+        }.start();
     }
 }
